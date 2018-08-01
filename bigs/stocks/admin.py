5fca8d6fa744f33297d11339212b419d0926d856
Jiyeong Yun <popsmile7@gmail.com>
2018-08-01 16:11:09 +0900

    Add models - Member, Stock

--- bigs/stocks/admin.py
+++ bigs/stocks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Member, StockCard
 
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(StockCard)
--- bigs/stocks/models.py
+++ bigs/stocks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+	name = models.CharField(max_length=10)
+	memberid = models.CharField(max_length=20)
+	password = models.CharField(max_length=16)
+	email = models.EmailField()
+	birth = models.DateTimeField()
+	phone = models.CharField(max_length=11)
+	gender = models.CharField(max_length=1)
+	tier = models.CharField(max_length=10)
+	TS_CREATED = models.DateTimeField()
+	TS_UPDATED = models.DateTimeField()
+	def __str__(self):
+		return self.name
+
+class StockCard(models.Model):
+	name = models.CharField(max_length=30)
+	information = models.TextField()
+	total_score = models.DecimalField(max_digits=3, decimal_places=1)
+	def __str__(self):
+		return self.name
