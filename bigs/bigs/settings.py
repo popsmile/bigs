79686e97e56dbe722381d1c7e279e213e325c766
Jiyeong Yun <popsmile7@gmail.com>
2018-08-03 12:58:53 +0900

    로그인 여부에 따른 종목명 노출여부 설정

--- bigs/bigs/settings.py
+++ bigs/bigs/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- bigs/stocks/views.py
+++ bigs/stocks/views.py
@@ -11,11 +11,3 @@ def index(request):
 	stocks4 = StockCard.objects.filter(category="테마주")
 	context = {'stocks1':stocks1, 'stocks2':stocks2, 'stocks3':stocks3, 'stocks4':stocks4}
 	return render(request, 'stocks/index.html', context)
-
-def stockcard(request):
-	stocks1 = StockCard.objects.filter(category="단기후보주")
-	stocks2 = StockCard.objects.filter(category="스윙")
-	stocks3 = StockCard.objects.filter(category="중장기")
-	stocks4 = StockCard.objects.filter(category="테마주")
-	context = {'stocks1':stocks1, 'stocks2':stocks2, 'stocks3':stocks3, 'stocks4':stocks4}
-	return render(request, 'stocks/stockcard.html', context)
