12bbc38e8415b326df78a5b6f16cdfe6e5532faf
Jiyeong Yun <popsmile7@gmail.com>
2018-08-03 11:39:19 +0900

    로그인 랜딩 수정 및 종목노출 카테고리별 추가

--- bigs/stocks/views.py
+++ bigs/stocks/views.py
@@ -5,8 +5,11 @@ from .models import StockCard
 
 # Create your views here.
 def index(request):
-	stocks = StockCard.objects.all()
-	context = {'stocks':stocks}
+	stocks1 = StockCard.objects.filter(category="단기후보주")
+	stocks2 = StockCard.objects.filter(category="스윙")
+	stocks3 = StockCard.objects.filter(category="중장기")
+	stocks4 = StockCard.objects.filter(category="테마주")
+	context = {'stocks1':stocks1, 'stocks2':stocks2, 'stocks3':stocks3, 'stocks4':stocks4}
 	return render(request, 'stocks/index.html', context)
 
 def stockcard(request):
