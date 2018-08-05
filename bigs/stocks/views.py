3898267c519058c354d435c5010fa2fca27d0ee9
Jiyeong Yun <popsmile7@gmail.com>
2018-08-05 13:25:53 +0900

    회원가입 페이지 생성 (차후 템플릿은 정돈필요)

--- bigs/stocks/views.py
+++ bigs/stocks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import StockCard
+from .forms import SignUpForm
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,14 @@ def index(request):
 	stocks4 = StockCard.objects.filter(category="테마주")
 	context = {'stocks1':stocks1, 'stocks2':stocks2, 'stocks3':stocks3, 'stocks4':stocks4}
 	return render(request, 'stocks/index.html', context)
+
+def signup(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			return render(request, 'registration/signup_done.html', {'username':username})
+	else:
+		form = SignUpForm()
+	return render(request, 'registration/signup.html', {'form':form})
